4ede05d8ea785a8058ed0e288a08d3e6b9902213
antu <shubhashishkar@gmail.com>
2024-02-06 09:23:36 -0800

    [add] filters added

--- filters.cpp
+++ filters.cpp
@@ -46,6 +46,49 @@ Filters::NLFFilter(const Graph *data_graph, const Graph *query_graph, ui **&cand
     return true;
 }
 
+void
+FilterVertices::computeCandidateWithNLF(const Graph *data_graph, const Graph *query_graph, VertexID query_vertex,
+                                        ui &count, ui *buffer) {
+
+    LabelID label = query_graph->getVertexLabel(query_vertex);
+    ui degree = query_graph->getVertexDegree(query_vertex);
+
+    ui data_vertex_num;
+    const ui* data_vertices = data_graph->getVerticesByLabel(label, data_vertex_num);
+
+    count = 0;
+    for (ui j = 0; j < data_vertex_num; ++j) {
+        ui data_vertex = data_vertices[j];
+        if (data_graph->getVertexDegree(data_vertex) >= degree) {
+
+            // NLF check
+            const std::unordered_map<LabelID, ui>* data_vertex_nlf = data_graph->getVertexNLF(data_vertex);
+
+            if (data_vertex_nlf->size() >= query_vertex_nlf->size()) { // checking whether the data vertex has more/equal neighbors or not
+                bool is_valid = true;
+
+                for (auto element : *query_vertex_nlf) {
+                    auto iter = data_vertex_nlf->find(element.first);
+                    if (iter == data_vertex_nlf->end() || iter->second < element.second) {
+                        is_valid = false;
+                        break;
+                    }
+                }
+
+                if (is_valid) {
+                    if (buffer != NULL) {
+                        buffer[count] = data_vertex;
+                    }
+                    count += 1;
+                }
+            }
+
+        }
+    }
+
+
+}
+
 
 
 
