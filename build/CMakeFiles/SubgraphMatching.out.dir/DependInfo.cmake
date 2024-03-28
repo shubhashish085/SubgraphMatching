c9713a7e65f3fa92beac5d4fc19d03bf33f7e33a
antu <shubhashishkar@gmail.com>
2024-03-28 07:42:14 -0700

    [add] correctness validated with CFL

---
+++ build/CMakeFiles/SubgraphMatching.out.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/antu/Research_Projects/SubgraphMatchingMain/Enumeration.cpp" "CMakeFiles/SubgraphMatching.out.dir/Enumeration.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/Enumeration.cpp.o.d"
+  "/home/antu/Research_Projects/SubgraphMatchingMain/FilterVertices.cpp" "CMakeFiles/SubgraphMatching.out.dir/FilterVertices.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/FilterVertices.cpp.o.d"
+  "/home/antu/Research_Projects/SubgraphMatchingMain/GeneratingFilterPlan.cpp" "CMakeFiles/SubgraphMatching.out.dir/GeneratingFilterPlan.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/GeneratingFilterPlan.cpp.o.d"
+  "/home/antu/Research_Projects/SubgraphMatchingMain/ParallelEnumeration.cpp" "CMakeFiles/SubgraphMatching.out.dir/ParallelEnumeration.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/ParallelEnumeration.cpp.o.d"
+  "/home/antu/Research_Projects/SubgraphMatchingMain/StudyPerformance.cpp" "CMakeFiles/SubgraphMatching.out.dir/StudyPerformance.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/StudyPerformance.cpp.o.d"
+  "/home/antu/Research_Projects/SubgraphMatchingMain/backtracking.cpp" "CMakeFiles/SubgraphMatching.out.dir/backtracking.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/backtracking.cpp.o.d"
+  "/home/antu/Research_Projects/SubgraphMatchingMain/graph.cpp" "CMakeFiles/SubgraphMatching.out.dir/graph.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/graph.cpp.o.d"
+  "/home/antu/Research_Projects/SubgraphMatchingMain/utilities.cpp" "CMakeFiles/SubgraphMatching.out.dir/utilities.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/utilities.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
