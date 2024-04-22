62dae855e47aaa715b4f6cef37b083f00c6d0528
Antu <shubhashishkar@gmail.com>
2024-04-22 13:54:59 -0700

    [add] analysis files added

--- StudyPerformance.cpp
+++ StudyPerformance.cpp
@@ -511,17 +511,19 @@ int main(int argc, char** argv) {
 
 int main(int argc, char** argv) {
 
-    std::string input_query_graph_file = "../tests/basic_query_graph_wo_label.graph";
-    //std::string input_query_graph_file = "../tests/4_node_graph_wo_label.graph";
+    //std::string input_query_graph_file = "../tests/basic_query_graph_wo_label.graph";
+    std::string input_query_graph_file = "../tests/4_node_graph_wo_label.graph";
     //std::string input_query_graph_file = "../tests/5_node_graph_wo_label.graph";
     //std::string input_data_graph_file = "../tests/basic_data_graph_wo_label.graph";
-    std::string input_data_graph_file = "/home/antu/Research_Projects/dataset/roadNet-CA.txt";
+
+    std::string input_data_graph_file = "/home/kars1/Parallel_computation/dataset/roadNet-CA.txt";
+
     //std::string input_query_graph_file = "../tests/basic_query_graph_wo_label.graph";
     //std::string input_data_graph_file = "../tests/formatted_graph_2048.graph";
     //std::string input_data_graph_file = "../tests/data_graph_4_wo_label.graph";
 
 
-    std::string output_file = "../analysis/sample_test.graph";
+    std::string output_file = "../analysis/analysis_roadnet_ca_query_4.graph";
 
     Graph* query_graph = new Graph();
     query_graph->loadGraphFromFile(input_query_graph_file);
