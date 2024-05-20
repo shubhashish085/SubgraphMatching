5fdea8bc02fb06a029943ea00c1b9b23ad291575
Antu <shubhashishkar@gmail.com>
2024-05-20 11:59:09 -0700

    [add] serial enumeration time evaluated

---
+++ build/CMakeFiles/SubgraphMatching.out.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SubgraphMatching.out.dir/Enumeration.cpp.o"
+  "CMakeFiles/SubgraphMatching.out.dir/Enumeration.cpp.o.d"
+  "CMakeFiles/SubgraphMatching.out.dir/FilterVertices.cpp.o"
+  "CMakeFiles/SubgraphMatching.out.dir/FilterVertices.cpp.o.d"
+  "CMakeFiles/SubgraphMatching.out.dir/GeneratingFilterPlan.cpp.o"
+  "CMakeFiles/SubgraphMatching.out.dir/GeneratingFilterPlan.cpp.o.d"
+  "CMakeFiles/SubgraphMatching.out.dir/ParallelEnumeration.cpp.o"
+  "CMakeFiles/SubgraphMatching.out.dir/ParallelEnumeration.cpp.o.d"
+  "CMakeFiles/SubgraphMatching.out.dir/StudyPerformance.cpp.o"
+  "CMakeFiles/SubgraphMatching.out.dir/StudyPerformance.cpp.o.d"
+  "CMakeFiles/SubgraphMatching.out.dir/backtracking.cpp.o"
+  "CMakeFiles/SubgraphMatching.out.dir/backtracking.cpp.o.d"
+  "CMakeFiles/SubgraphMatching.out.dir/commandparser.cpp.o"
+  "CMakeFiles/SubgraphMatching.out.dir/commandparser.cpp.o.d"
+  "CMakeFiles/SubgraphMatching.out.dir/graph.cpp.o"
+  "CMakeFiles/SubgraphMatching.out.dir/graph.cpp.o.d"
+  "CMakeFiles/SubgraphMatching.out.dir/matchingcommand.cpp.o"
+  "CMakeFiles/SubgraphMatching.out.dir/matchingcommand.cpp.o.d"
+  "CMakeFiles/SubgraphMatching.out.dir/utilities.cpp.o"
+  "CMakeFiles/SubgraphMatching.out.dir/utilities.cpp.o.d"
+  "SubgraphMatching.out"
+  "SubgraphMatching.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SubgraphMatching.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
