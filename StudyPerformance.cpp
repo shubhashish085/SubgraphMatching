20f900d6e0cd3aebad83e7ac44a8bea030a24b81
Antu <shubhashishkar@gmail.com>
2025-02-24 20:43:26 -0800

    [add] kron related code added

--- StudyPerformance.cpp
+++ StudyPerformance.cpp
@@ -506,13 +506,13 @@ int main(int argc, char** argv) {
 
 int main(int argc, char** argv) {
 
-    std::string input_query_graph_file = "../tests/basic_query_graph_wo_label.graph";
-    //std::string input_query_graph_file = "../tests/4_node_graph_wo_label.graph";
+    //std::string input_query_graph_file = "../tests/basic_query_graph_wo_label.graph";
+    std::string input_query_graph_file = "../tests/4_node_graph_wo_label.graph";
     //std::string input_query_graph_file = "../tests/5_node_graph_wo_label.graph";
     //std::string input_data_graph_file = "../tests/basic_data_graph_wo_label.graph";
-    std::string input_data_graph_file = "/home/antu/Research_Projects/dataset/com-amazon.ungraph.txt";
+    std::string input_data_graph_file = "/home/kars1/Parallel_computation/dataset/com-youtube.ungraph.txt";
 
-    std::string output_file = "../analysis/analysis_amazon_query_3.graph";
+    std::string output_file = "../analysis/analysis_youtube_query_4.graph";
 
     Graph* query_graph = new Graph();
     query_graph->loadGraphFromFile(input_query_graph_file);
---
+++ build/CMakeFiles/SubgraphMatching.out.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kars1/Research_Projects/SubgraphMatching/Enumeration.cpp" "CMakeFiles/SubgraphMatching.out.dir/Enumeration.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/Enumeration.cpp.o.d"
+  "/home/kars1/Research_Projects/SubgraphMatching/FilterVertices.cpp" "CMakeFiles/SubgraphMatching.out.dir/FilterVertices.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/FilterVertices.cpp.o.d"
+  "/home/kars1/Research_Projects/SubgraphMatching/GeneratingFilterPlan.cpp" "CMakeFiles/SubgraphMatching.out.dir/GeneratingFilterPlan.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/GeneratingFilterPlan.cpp.o.d"
+  "/home/kars1/Research_Projects/SubgraphMatching/ParallelEnumeration.cpp" "CMakeFiles/SubgraphMatching.out.dir/ParallelEnumeration.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/ParallelEnumeration.cpp.o.d"
+  "/home/kars1/Research_Projects/SubgraphMatching/StudyPerformance.cpp" "CMakeFiles/SubgraphMatching.out.dir/StudyPerformance.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/StudyPerformance.cpp.o.d"
+  "/home/kars1/Research_Projects/SubgraphMatching/backtracking.cpp" "CMakeFiles/SubgraphMatching.out.dir/backtracking.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/backtracking.cpp.o.d"
+  "/home/kars1/Research_Projects/SubgraphMatching/graph.cpp" "CMakeFiles/SubgraphMatching.out.dir/graph.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/graph.cpp.o.d"
+  "/home/kars1/Research_Projects/SubgraphMatching/utilities.cpp" "CMakeFiles/SubgraphMatching.out.dir/utilities.cpp.o" "gcc" "CMakeFiles/SubgraphMatching.out.dir/utilities.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
